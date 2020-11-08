2a92ef691c0a7a21bbc0327d7eb374bedf9f74b8
MedaniGunathilaka <hansikamedani@gmail.com>
2020-11-08 11:13:06 +0530

    Added view spots and view societies files

---
+++ public/office/O_ViewSocieties.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Inquieries</title>
+<script src="../js/jquery-1.9.1.min.js"></script>
+<script src="../js/nav.js"></script>
+<link rel="stylesheet" href="../css/view.css " type="text/css">
+<link type="text/css" rel="stylesheet" href="../css/main.css">
+<link type="text/css" rel="stylesheet" href="../css/inquiry.css">
+</head>
+<body>
+<div id="nav"></div>
+		
+		<div class="content">
+		
+			<h1>SPORTS</h1>
+			
+		
+			<div class="card">
+			
+				<br>
+				<br>
+			</div>
+			  <div class="card">
+				<hr>
+				<table>
+					<tr>
+						<th>Society ID</th>
+						<th>Society</th>
+						<th>Teacher incharge ID</th>
+						<th>Number of Students</th>
+						
+					</tr>
+					<tr>
+						<td>01</td>
+						<td>Arts</td>
+						<td>W.H.M.Gunathilaka</td>
+						<td>20</td>
+						
+					</tr>
+					<tr>
+						<td>02</td>
+						<td>Dancing</td>
+						<td>M.M.Gunathilaka</td>
+						<td>40</td>
+						
+					</tr>
+					
+					
+					
+				</table>
+				
+				</div>
+				
+				
+		</div>
+		
+</body>
+</html>
