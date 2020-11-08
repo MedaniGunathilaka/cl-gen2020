486944d138473a858539cdc9b8f32b4ae8e98154
MedaniGunathilaka <hansikamedani@gmail.com>
2020-11-08 09:48:30 +0530

    added sports2 file to admin

---
+++ public/admin/A_Sports2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Sports</title>
+<script src="../js/jquery-1.9.1.min.js"></script>
+<script src="../js/nav.js"></script>
+<link rel="stylesheet" href="../css/view.css " type="text/css">
+<link type="text/css" rel="stylesheet" href="../css/main.css">
+<link type="text/css" rel="stylesheet" href="../css/cat_form.css">
+</head>
+<body>
+	<div id="nav"></div>
+		
+		<div class="content">
+		
+			
+			
+		
+			<div class="card">
+  
+				<div class="container">
+					<h2><b>SPORTS</b></h2>
+					
+					<form action="../../src/A_addsports.php" method="POST">
+					
+						<hr>
+						<label for="name"><b>Sport ID</b></label>
+						<input type="text" name="Spid" required>
+						
+						<label for="name"><b>Sport</b></label>
+						<input type="text"  name="Sname" required>
+
+						<label for="TID"><b>Teacher in charge ID</b></label>
+						<input type="text"  name="TID" required>
+						
+						<label for="TID"><b>Number of Students</b></label>
+						<input type="text"  name="NOS" required>
+						
+						<hr>
+					
+						<button type="submit" class="registerbtn" name="regbtn">Add</button>
+					</form>
+					<button type="submit" class="deletebtn">Delete</button>
+				</div>
+				
+			</div>
+		</div>
+		
+</body>
+</html>
