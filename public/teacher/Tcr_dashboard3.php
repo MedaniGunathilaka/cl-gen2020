262ee74ea3315d2a0d9b2d43253d9a61cb40d031
MedaniGunathilaka <hansikamedani@gmail.com>
2020-11-12 23:07:48 +0530

    Added Tcr_dashboard3

---
+++ public/teacher/Tcr_dashboard3.php
@@ -0,0 +1,115 @@
+
+<!-- <?php
+    // session_start();
+
+    // if(!isset($_SESSION['userType'])){
+    //     $error = "Please Login!";
+    //     header('Location: ../common/loginFile.php?error='.$error);
+    // }elseif($_SESSION['userType'] == 'teacher'){
+
+    //     $userID = $_SESSION['userID'];
+    ?> -->
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Inquieries 1</title>
+          <script src="../js/jquery-1.9.1.min.js"></script>
+        <script src="../js/nav.js"></script>
+        <link type="text/css" rel="stylesheet" href="../css/main.css">
+        <link type="text/css" rel="stylesheet" href="../css/register2.css">
+        <link type="text/css" rel="stylesheet" href="../css/teacher.css">
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    </head>
+    <body>
+    
+    
+    <div id="nav3"></div>
+            
+            <div class="content">
+                <h2>TEACHER</h2>
+                    <div class="card">
+                    
+                    <div class="r-container">
+            
+                  <img src = "../../images/2.png" width="100" height="100">
+                    <h3><b><a href="Tcr_grades3.php">Upload Marks</a></b></h3>
+            </div>
+                 
+            <div class="r-container">
+        
+                  <img src = "../../images/1.png" width="110" height="100">
+                    <h3><b><a href="Tcr_position2.php">Generate Positions</a></b></h3>
+                    
+            </div>
+            </div>
+            </br>
+            <h2>     </h2>	
+            
+            <div class="card">
+            <div class="r-container">
+                  <img src = "../../images/3.jpg" width="110" height="100">
+                    <h3><b><a href="Tcr_AddInquiery3.php">Inquiries and Replies</a></b></h3>
+                </div>
+                
+                <div class="r-container">
+                  <img src = "../../images/4.jpg" width="110" height="100">
+                    <h3><b><a href="Tcr_fees4.php">Fees and Fines</a></b></h3>
+                </div>
+            </div>
+                  </br>
+                
+                <h2>  </h2>	
+
+                <div class="card">
+				
+		<div class="l-container">
+			  <img src = "../../images/11.jpg" width="100" height="100">
+				<h3><b><a href="Tcr_achievement2.php">Cricket</a></b></h3>
+		</div>
+			 
+			 
+		<div class="l-container">
+			<img src = "../../images/13.jpg" width="110" height="100">
+				<h3><b><a href="Tcr_achievement2.php">Volley ball</a></b></h3>
+				
+		</div>
+		</div>
+			
+		</br>
+		<h2>     </h2>	
+		<div class="card">
+			<div class="r-container">
+			  <img src = "../../images/12.png" width="110" height="100">
+				<h3><b><a href="Tcr_achievement2.php">Tamil Society</a></b></h3>
+			</div>
+			
+		<div class="r-container">
+			  <img src = "../../images/9.jpg" width="110" height="100">
+				<h3><b><a href="Tcr_achievement2.php">Oriental Music Society</a></b></h3>
+		</div>
+		</div>
+	
+		</br>
+		<h2>     </h2>	
+            
+                <div class="card">
+                    <div class="r-container">
+                    <img src = "../../images/5.jpg" width="110" height="100">
+                        <h3><b><a href="Tcr_profile3.php">Profile</a></b></h3>
+                    </div>
+                    
+                <div class="r-container">
+                  <img src = "../../images/6.jpg" width="110" height="100">
+                    <h3><b><a href="newsfeed3.php">News Feed</a></b></h3>
+                </div>
+            </div>
+            </div>
+    </body>
+    </html>	
+    
+    <?php 
+    // } 
+    ?>
